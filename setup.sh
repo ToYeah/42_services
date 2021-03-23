f026b7c779591adaa5df48b36759da80b1d43aba
Taisei Toya <tytaisei0102@gmail.com>
2021-03-23 18:43:26 +0100

    feat(metallb): metallbの設定を追加

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+
 minikube delete
 
-minikube start
+minikube start --driver=docker --cpus=4 --memory='4g'
 
 eval $(minikube docker-env)
 
-docker build -t ft_nginx ./srcs/nginx/
+docker build -t ft_nginx srcs/nginx
 
-kubectl apply -f ./srcs/nginx/ft_nginx.yaml
+minikube addons enable metallb
+
+kubectl apply -f srcs/metallb.yml
+
+kubectl apply -f srcs/nginx/ft_nginx.yaml
