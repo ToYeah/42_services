14647f35c7d045ed8a736975cbf910d14ef298f3
Taisei Toya <tytaisei0102@gmail.com>
2021-03-24 11:30:35 +0100

    feat(ft_wordpress): wordpressコンテナを追加

--- setup.sh
+++ setup.sh
@@ -7,9 +7,11 @@ minikube start --driver=docker --cpus=4 --memory='4g'
 eval $(minikube docker-env)
 
 docker build -t ft_nginx srcs/nginx
+docker build -t ft_wordpress srcs/wordpress
 
 minikube addons enable metallb
 
 kubectl apply -f srcs/metallb.yml
 
-kubectl apply -f srcs/nginx/ft_nginx.yaml
+kubectl apply -f srcs/nginx
+kubectl apply -f srcs/wordpress
